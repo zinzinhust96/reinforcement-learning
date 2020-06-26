7828aa6def2319a5a5e74a486fbe4f7bf6065561
Nam Doan Ngoc <giang.doanngoc@arsvietnam.com>
2020-06-26 11:13:06 +0700

    dqn, attempt to train breakout agent

---
+++ DQN/inference.py
@@ -0,0 +1,283 @@
+import gym
+import numpy as np
+from matplotlib import pyplot as plt
+import time
+import tensorflow as tf
+import os
+import itertools
+
+env = gym.envs.make("Breakout-v0")
+
+# Atari Actions: 0 (noop), 1 (fire), 2 (left) and 3 (right) are valid actions
+VALID_ACTIONS = [0, 1, 2, 3]
+
+class StateProcessor():
+    """
+    Processes a raw Atari images. Resizes it and converts it to grayscale.
+    """
+    def __init__(self):
+        # Build the Tensorflow graph
+        with tf.variable_scope("state_processor"):
+            self.input_state = tf.placeholder(shape=[210, 160, 3], dtype=tf.uint8)
+            self.output = tf.image.rgb_to_grayscale(self.input_state)
+            self.output = tf.image.crop_to_bounding_box(self.output, 34, 0, 160, 160)
+            self.output = tf.image.resize_images(
+                self.output, [84, 84], method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
+            self.output = tf.squeeze(self.output)
+
+    def process(self, sess, state):
+        """
+        Args:
+            sess: A Tensorflow session object
+            state: A [210, 160, 3] Atari RGB State
+
+        Returns:
+            A processed [84, 84] state representing grayscale values.
+        """
+        return sess.run(self.output, { self.input_state: state })
+
+class Estimator():
+    """Q-Value Estimator neural network.
+
+    This network is used for both the Q-Network and the Target Network.
+    """
+
+    def __init__(self, scope="estimator", summaries_dir=None):
+        self.scope = scope
+        # Writes Tensorboard summaries to disk
+        self.summary_writer = None
+        with tf.variable_scope(scope):
+            # Build the graph
+            self._build_model_mod()
+            if summaries_dir:
+                summary_dir = os.path.join(summaries_dir, "summaries_{}".format(scope))
+                if not os.path.exists(summary_dir):
+                    os.makedirs(summary_dir)
+                self.summary_writer = tf.summary.FileWriter(summary_dir)
+
+    def _build_model(self):
+        """
+        Builds the Tensorflow graph.
+        """
+
+        # Placeholders for our input
+        # Our input are 4 RGB frames of shape 160, 160 each
+        self.X_pl = tf.placeholder(shape=[None, 84, 84, 4], dtype=tf.uint8, name="X")
+        # The TD target value
+        self.y_pl = tf.placeholder(shape=[None], dtype=tf.float32, name="y")
+        # Integer id of which action was selected
+        self.actions_pl = tf.placeholder(shape=[None], dtype=tf.int32, name="actions")
+
+        X = tf.to_float(self.X_pl) / 255.0
+        batch_size = tf.shape(self.X_pl)[0]
+
+        # Three convolutional layers
+        conv1 = tf.contrib.layers.conv2d(
+            X, 32, 8, 4, activation_fn=tf.nn.relu)
+        conv2 = tf.contrib.layers.conv2d(
+            conv1, 64, 4, 2, activation_fn=tf.nn.relu)
+        conv3 = tf.contrib.layers.conv2d(
+            conv2, 64, 3, 1, activation_fn=tf.nn.relu)
+
+        # Fully connected layers
+        flattened = tf.contrib.layers.flatten(conv3)
+        fc1 = tf.contrib.layers.fully_connected(flattened, 512)
+        self.predictions = tf.contrib.layers.fully_connected(fc1, len(VALID_ACTIONS))
+
+        # Get the predictions for the chosen actions only
+        gather_indices = tf.range(batch_size) * tf.shape(self.predictions)[1] + self.actions_pl
+        self.action_predictions = tf.gather(tf.reshape(self.predictions, [-1]), gather_indices)
+
+        # Calculate the loss
+        self.losses = tf.squared_difference(self.y_pl, self.action_predictions)
+        self.loss = tf.reduce_mean(self.losses)
+
+        # Optimizer Parameters from original paper
+        self.optimizer = tf.train.RMSPropOptimizer(0.00025, 0.99, 0.0, 1e-6)
+        self.train_op = self.optimizer.minimize(self.loss, global_step=tf.contrib.framework.get_global_step())
+
+        # Summaries for Tensorboard
+        self.summaries = tf.summary.merge([
+            tf.summary.scalar("loss", self.loss),
+            tf.summary.histogram("loss_hist", self.losses),
+            tf.summary.histogram("q_values_hist", self.predictions),
+            tf.summary.scalar("max_q_value", tf.reduce_max(self.predictions))
+        ])
+
+    def _build_model_mod(self):
+        """
+        Builds the Tensorflow graph.
+        """
+
+        # Placeholders for our input
+        # Our input are 4 RGB frames of shape 160, 160 each
+        self.X_pl = tf.placeholder(shape=[None, 84, 84, 4], dtype=tf.uint8, name="X")
+        # The TD target value
+        self.y_pl = tf.placeholder(shape=[None], dtype=tf.float32, name="y")
+        # Integer id of which action was selected
+        self.actions_pl = tf.placeholder(shape=[None], dtype=tf.int32, name="actions")
+
+        X = tf.to_float(self.X_pl) / 255.0
+        batch_size = tf.shape(self.X_pl)[0]
+
+        # Three convolutional layers
+        conv1 = tf.layers.conv2d(
+            inputs=X, filters=32, kernel_size=[8, 8], strides=4,
+            kernel_initializer=tf.variance_scaling_initializer(scale=2),
+            padding="valid", activation=tf.nn.relu, use_bias=False)
+        conv2 = tf.layers.conv2d(
+            inputs=conv1, filters=64, kernel_size=[4, 4], strides=2, 
+            kernel_initializer=tf.variance_scaling_initializer(scale=2),
+            padding="valid", activation=tf.nn.relu, use_bias=False)
+        conv3 = tf.layers.conv2d(
+            inputs=conv2, filters=64, kernel_size=[3, 3], strides=1, 
+            kernel_initializer=tf.variance_scaling_initializer(scale=2),
+            padding="valid", activation=tf.nn.relu, use_bias=False)
+
+        # Fully connected layers
+        flattened = tf.contrib.layers.flatten(conv3)
+        fc1 = tf.contrib.layers.fully_connected(flattened, 512)
+        self.predictions = tf.contrib.layers.fully_connected(fc1, len(VALID_ACTIONS))
+
+        # Get the predictions for the chosen actions only
+        # gather_indices = tf.range(batch_size) * tf.shape(self.predictions)[1] + self.actions_pl
+        # self.action_predictions = tf.gather(tf.reshape(self.predictions, [-1]), gather_indices)
+
+        # Q value of the action that was performed (check it again)
+        self.action_predictions = tf.reduce_sum(tf.multiply(self.predictions, tf.one_hot(self.actions_pl, tf.shape(self.predictions)[1], dtype=tf.float32)), axis=1)
+
+        # Calculate the loss
+        self.losses = tf.losses.huber_loss(self.y_pl, self.action_predictions)
+        self.loss = tf.reduce_mean(self.losses)
+
+        # Optimizer Parameters from original paper
+        self.optimizer = tf.train.AdamOptimizer(learning_rate=0.00001)
+        self.train_op = self.optimizer.minimize(self.loss, global_step=tf.contrib.framework.get_global_step())
+
+        # Summaries for Tensorboard
+        self.summaries = tf.summary.merge([
+            tf.summary.scalar("loss", self.loss),
+            tf.summary.histogram("loss_hist", self.losses),
+            tf.summary.histogram("q_values_hist", self.predictions),
+            tf.summary.scalar("max_q_value", tf.reduce_max(self.predictions))
+        ])
+
+    def predict(self, sess, s):
+        """
+        Predicts action values.
+
+        Args:
+          sess: Tensorflow session
+          s: State input of shape [batch_size, 4, 160, 160, 3]
+
+        Returns:
+          Tensor of shape [batch_size, NUM_VALID_ACTIONS] containing the estimated 
+          action values.
+        """
+        return sess.run(self.predictions, { self.X_pl: s })
+
+    def update(self, sess, s, a, y):
+        """
+        Updates the estimator towards the given targets.
+
+        Args:
+          sess: Tensorflow session object
+          s: State input of shape [batch_size, 4, 160, 160, 3]
+          a: Chosen actions of shape [batch_size]
+          y: Targets of shape [batch_size]
+
+        Returns:
+          The calculated loss on the batch.
+        """
+        feed_dict = { self.X_pl: s, self.y_pl: y, self.actions_pl: a }
+        summaries, global_step, _, loss = sess.run(
+            [self.summaries, tf.contrib.framework.get_global_step(), self.train_op, self.loss],
+            feed_dict)
+        if self.summary_writer:
+            self.summary_writer.add_summary(summaries, global_step)
+        return loss
+
+def make_epsilon_greedy_policy(estimator, nA):
+    """
+    Creates an epsilon-greedy policy based on a given Q-function approximator and epsilon.
+
+    Args:
+        estimator: An estimator that returns q values for a given state
+        nA: Number of actions in the environment.
+
+    Returns:
+        A function that takes the (sess, observation, epsilon) as an argument and returns
+        the probabilities for each action in the form of a numpy array of length nA.
+
+    """
+    def policy_fn(sess, observation, epsilon):
+        A = np.ones(nA, dtype=float) * epsilon / nA
+        q_values = estimator.predict(sess, np.expand_dims(observation, 0))[0]
+        best_action = np.argmax(q_values)
+        A[best_action] += (1.0 - epsilon)
+        return A
+    return policy_fn
+
+# Where we save our checkpoints and graphs
+experiment_dir = os.path.abspath("./experiments/{}".format(env.spec.id))
+checkpoint_dir = os.path.join(experiment_dir, "checkpoints")
+print('checkpoint_dir: ', checkpoint_dir)
+
+# Create estimators
+q_estimator = Estimator(scope="q", summaries_dir=experiment_dir)
+
+# State processor
+state_processor = StateProcessor()
+
+# num_episodes
+num_episodes = 1
+
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    # Keeps track of useful statistics
+    episode_lengths = np.zeros(num_episodes)
+    episode_rewards = np.zeros(num_episodes)
+
+    saver = tf.train.Saver()
+    # Load a previous checkpoint if we find one
+    # latest_checkpoint = tf.train.latest_checkpoint(checkpoint_dir)
+    # if latest_checkpoint:
+    #     print("Loading model checkpoint {}...\n".format(latest_checkpoint))
+        # saver.restore(sess, latest_checkpoint)
+    saver.restore(sess, '/home/namdng/Documents/RL/reinforcement-learning/DQN/experiments/Breakout-v0/checkpoints/model')
+
+    # The policy we're following
+    policy = make_epsilon_greedy_policy(
+        q_estimator,
+        len(VALID_ACTIONS))
+
+    for i_episode in range(num_episodes):
+        state = env.reset()
+        state = state_processor.process(sess, state)
+        state = np.stack([state] * 4, axis=2)
+        print(state.shape)
+        for t in itertools.count():
+            env.render()
+
+            # Take a step (best action)
+            action_probs = policy(sess, state, epsilon=0)   # greedy policy
+            print(action_probs)
+            action = np.random.choice(np.arange(len(action_probs)), p=action_probs)
+            next_state, reward, done, _ = env.step(VALID_ACTIONS[action])
+            next_state = state_processor.process(sess, next_state)
+            next_state = np.append(state[:,:,1:], np.expand_dims(next_state, 2), axis=2)
+
+            # Update statistics
+            episode_rewards[i_episode] += reward
+            episode_lengths[i_episode] = t
+
+            if done:
+                print("Episode finished after {} timesteps".format(t+1))
+                break
+
+            state = next_state
+            # time.sleep(0.01)
+
+        print("\nEpisode Reward: {}".format(episode_rewards[i_episode]))
+
+    env.close()
